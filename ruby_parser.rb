00ff7c0a396deda0e4cdfa41887948bed9f46c3f
davygora <glory-gerrard@ukr.net>
2016-03-26 22:35:07 +0200

    Fix error

--- ruby_parser.rb
+++ ruby_parser.rb
@@ -13,18 +13,18 @@ doc.css('.showing').each do |showing|
   showing_id = showing['id'].split('_').last.to_i
   tags = showing.css('.tags a').map { |tag| tag.text.strip }
   title_el = showing.at_css('h1 a')
-  title_el.children.each { |c| c.remove if c.name == 'span' }
-  title = title_el.text.split
+  title_el.children.each { |c| c.remove if c.name == "span" }
+  title = title_el.text.strip
   dates = showing.at_css('.start_and_pricing').inner_html.strip
   dates = dates.split('<br>').map(&:strip).map { |d| DateTime.parse(d) }
   description = showing.at_css('.copy').text.gsub('more...', '').strip
   showings.push(
     id: showing_id,
-    title: title_el,
+    title: title,
     tags: tags,
     dates: dates,
     description: description
     )
+end
 
 File.open("result.txt", "w") {|file| file.puts JSON.pretty_generate(showings) }
-end
