0b0d5f73db0f4e14581a9b5f0a7ee41bf7ab3d87
davygora <glory-gerrard@ukr.net>
2016-03-26 21:46:46 +0200

    Create parse some page

---
+++ ruby_parser.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+
+url = 'http://www.cubecinema.com/programme'
+html = open(url)
+
+doc = Nokogiri::HTML(html)
+
+showings = Array.new
+
+doc.css('.showing').each do |showing|
+  showing_id = showing['id'].split('_').last.to_i
+  tags = showing.css('.tags a').map { |tag| tag.text.strip }
+  title_el = showing.at_css('h1 a')
+  title_el.children.each { |c| c.remove if c.name == 'span' }
+  title = title_el.text.split
+  dates = showing.at_css('.start_and_pricing').inner_html.strip
+  dates = dates.split('<br>').map(&:strip).map { |d| DateTime.parse(d) }
+  description = showing.at_css('.copy').text.gsub('more...', '').strip
+  showings.push(
+    id: showing_id,
+    title: title_el,
+    tags: tags,
+    dates: dates,
+    description: description
+    )
+
+File.open("result.txt", "w") {|file| file.puts JSON.pretty_generate(showings) }
+end
