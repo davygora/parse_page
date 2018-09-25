4d131d116a10f64f80b105a378bc0e14ffff9897
Eugene Gladyshev <davygora.e@gmail.com>
2018-09-25 14:00:00 +0100

    Refactor

--- parser_hacker_news.rb
+++ parser_hacker_news.rb
@@ -2,9 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-results      = []
-titles_links = []
-authors      = []
+results, titles_links, authors = [], [], []
 
 (1..5).each do |i|
   doc = Nokogiri::HTML(open("https://news.ycombinator.com/news?p=#{i}").read)
@@ -12,20 +10,18 @@ authors      = []
   doc.css('.athing').each do |article|
     title = article.css('.title .storylink').text
     href = article.css('.storylink')
-    links = href.map{ |l| l['href'] }
+    links = href.map{|l| l['href']}
 
     titles_links.push(
       title: title,
-      url: links
-    )
+      url: links)
   end
 
   doc.css('.subtext .hnuser').each do |name|
     authors.push(
-      author: name.text
-    )
+      author: name.text)
   end
 end
 
-results = titles_links.map.with_index { |e, i| e.merge(authors[i]) }
-File.open("hacke_news.txt", "w") { |file| file.puts JSON.pretty_generate(results) }
+results = titles_links.map.with_index {|e,i| e.merge(authors[i])}
+File.open("hacke_news.txt", "w") {|file| file.puts JSON.pretty_generate(results)}
